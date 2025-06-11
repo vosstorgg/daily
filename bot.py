ec6b54d909c17a715cad8253b3f1cf930a43f0d5
vosstorgg <ryzhkow@gmail.com>
2025-06-11 11:00:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
+import asyncio
 from telegram.ext import ApplicationBuilder
 from handlers import register_handlers
 from config import BOT_TOKEN
 from db import init_db
-import asyncio
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-register_handlers(app)
+async def main():
+    await init_db()  # создаём таблицу, если ещё нет
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    register_handlers(app)
+    await app.run_polling()  # запускаем бота
 
 if __name__ == "__main__":
-    asyncio.run(init_db())  # создаёт таблицу, если её ещё нет
-    app.run_polling()
+    asyncio.run(main())
