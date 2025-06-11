f6eddf686d7d7ee415ece8735899a06a0159c4e0
vosstorgg <ryzhkow@gmail.com>
2025-06-11 10:40:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,9 @@
-print("✅ Бот запущен")
-
 from telegram.ext import ApplicationBuilder
 from handlers import register_handlers
 from config import BOT_TOKEN
-import asyncio
 
-async def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    register_handlers(app)
-    await app.run_polling()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+register_handlers(app)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run_polling()
