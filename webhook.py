d6103eec8921e37753fba2505c223acd2e280f30
vosstorgg <ryzhkow@gmail.com>
2025-06-14 17:44:14 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,11 +1,10 @@
+# webhook.py
 import os
 from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-@app.route('/webhook', methods=['POST'])
-async def webhook():
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    asyncio.create_task(application.process_update(update))  # обработка в фоне
-    return 'ok', 200  # ⏱️ отвечаем сразу
+async def setup_webhook():
+    bot = Bot(BOT_TOKEN)
+    await bot.set_webhook(url=WEBHOOK_URL)
