e246467c794eefde2aacd2e3cc50e98155efe71d
vosstorgg <ryzhkow@gmail.com>
2025-06-14 17:26:21 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,7 @@ from telegram.ext import CommandHandler, ContextTypes
 from gpt_client import ask_gpt  # not used yet
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(">> /start triggered")
     await update.message.reply_text("Hello, world!")
 
 start_handler = CommandHandler("start", start)
